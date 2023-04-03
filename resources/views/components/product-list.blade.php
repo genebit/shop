945fb63b1bfdd0010ba04ce287235e5fc75cda29
genebit <genebitara@gmail.com>
2023-04-03 19:04:26 +0800

    Initial commit (part 1)

---
+++ resources/views/components/product-list.blade.php
@@ -0,0 +1,31 @@
+<div class="container-fluid py-5">
+    <div class="col-12 mx-auto">
+        <h1 class="text-center">Our Products</h1>
+        <div class="row row-cols-4 gap-5 justify-content-center">
+            <div class="card col p-0">
+                <img src="{{ asset('img/fab.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">Classic Denim</h4>
+                    <p class="card-text">P499.00</p>
+                    <a href="#" class="btn-dark-green text-decoration-none">Buy now</a>
+                </div>
+            </div>
+            <div class="card col p-0">
+                <img src="{{ asset('img/spring.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">Classic Denim</h4>
+                    <p class="card-text">P499.00</p>
+                    <a href="#" class="btn-dark-green text-decoration-none">Buy now</a>
+                </div>
+            </div>
+            <div class="card col p-0">
+                <img src="{{ asset('img/summer.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">Classic Denim</h4>
+                    <p class="card-text">P499.00</p>
+                    <a href="#" class="btn-dark-green text-decoration-none">Buy now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
